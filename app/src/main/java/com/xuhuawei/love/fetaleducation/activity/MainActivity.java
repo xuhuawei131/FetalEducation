6944ceecaca675c981b9ef38c405093b1879aee4
xuhuawei <xuhw@knowbox.cn>
2018-11-14 23:02:32 +0800

    init

---
+++ app/src/main/java/com/xuhuawei/love/fetaleducation/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xuhuawei.love.fetaleducation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xuhuawei.love.fetaleducation.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
--- app/src/main/java/com/xuhuawei/love/fetaleducation/activity/StartActivity.java
+++ app/src/main/java/com/xuhuawei/love/fetaleducation/activity/StartActivity.java
@@ -1,18 +1,21 @@
 package com.xuhuawei.love.fetaleducation.activity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.xhwbaselibrary.base.BasePermissActivity;
+import com.xhwbaselibrary.permission.MyPermissionTask;
 import com.xuhuawei.love.fetaleducation.R;
 
 import java.security.Permission;
 
 public class StartActivity extends BasePermissActivity {
-private static final String PERMISSIO_ARRAY[]=new String[]{
-        Manifest.permission.READ_PHONE_STATE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION, };
+    private static final String PERMISSIO_ARRAY[] = new String[]{
+            Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION,};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +27,22 @@ private static final String PERMISSIO_ARRAY[]=new String[]{
         }
     }
 
-    private  void checkAndRequestPermission() {
-        checkRuntimePermission(PERMISSIO_ARRAY);
+    private void checkAndRequestPermission() {
+        checkRuntimePermission(new MyPermissionTask(PERMISSIO_ARRAY) {
+            @Override
+            public void onPermissionDenied(String[] var1) {
+
+            }
+
+            @Override
+            public void allPermissionGranted() {
+                fetchSplashAD();
+            }
+        });
     }
 
     private void fetchSplashAD() {
-
+        startActivity(new Intent(this,MainActivity.class));
     }
 
 
